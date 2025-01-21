c2f6458dc3a8234948e6892208f253ff7f952a17
Bengt Fredh <bengt.rino.fredh@digdir.no>
2025-01-21 13:57:32 +0100

    Update github actions

--- infra/altinn-digdir-oed-tt02-rg/subnet.tf
+++ infra/altinn-digdir-oed-tt02-rg/subnet.tf
@@ -5,7 +5,7 @@ resource "azurerm_subnet" "default" {
   private_endpoint_network_policies             = "Disabled"
   private_link_service_network_policies_enabled = true
   resource_group_name                           = azurerm_resource_group.rg.name
-  virtual_network_name = "oed-${var.environment}-feedpoller-vnet"
+  virtual_network_name                          = "oed-${var.environment}-feedpoller-vnet"
   delegation {
     name = "delegation"
     service_delegation {
