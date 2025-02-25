5c234d530a8549dab08f2df6c556ebb25b82f020
Kurt Monge <kurt.monge@knowit.com>
2025-02-25 12:27:25 +0100

    Lagt på ip begrensning til storge

--- infra/altinn-digdir-oed-rg/storage.tf
+++ infra/altinn-digdir-oed-rg/storage.tf
@@ -7,6 +7,11 @@ resource "azurerm_storage_account" "sa" {
   blob_properties {
     versioning_enabled = true
   }
+  network_rules {
+    default_action             = "Deny"
+    ip_rules                   = ["100.0.0.1"]
+    virtual_network_subnet_ids = [azurerm_subnet.default.id]
+  }
 }
 
 resource "random_password" "psql_oedpgadmin" {
