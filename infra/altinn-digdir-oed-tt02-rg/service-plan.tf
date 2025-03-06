644717df5d8707b18bea126b5818c5e27afa015e
Kurt Monge <kurt.monge@digdir.no>
2025-03-06 13:18:04 +0100

    Oppdaterte serviceplan på authz

--- infra/altinn-digdir-oed-tt02-rg/service-plan.tf
+++ infra/altinn-digdir-oed-tt02-rg/service-plan.tf
@@ -3,7 +3,7 @@ resource "azurerm_service_plan" "feedpoller" {
   location            = var.alt_location
   resource_group_name = azurerm_resource_group.rg.name
   os_type             = "Windows"
-  sku_name            = "B2"
+  sku_name            = "EP1"
   worker_count        = 1
   tags = {
     costcenter = "altinn3"
