e54e8562fc14befc574ffbdf356786093fd6eb91
Kurt Monge <kurt.monge@knowit.com>
2025-03-26 11:43:58 +0100

    Rettet for langt kortnavn

--- infra/altinn-digdir-oed-rg/alert.tf
+++ infra/altinn-digdir-oed-rg/alert.tf
@@ -1,7 +1,7 @@
 resource "azurerm_monitor_action_group" "email_ag" {
   name                = "DD-email-notification-prod-ag"
   resource_group_name = azurerm_resource_group.rg.name
-  short_name          = "E-mail DD - prod"
+  short_name          = "E-mail DD-p"
   email_receiver {
     email_address = var.support_email
     name          = "send-to-EmailAction"
