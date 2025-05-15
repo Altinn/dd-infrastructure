158b00efb6a9f363d3fc4dc4b40fa64e65c8a3ad
Kurt70 <kurt.monge@gmail.com>
2025-05-15 13:28:30 +0200

    1181: Lagt til whitelisting og utvidede rettigheter til github principal

--- infra/altinn-digdir-oed-rg/terraform.tfvars
+++ infra/altinn-digdir-oed-rg/terraform.tfvars
@@ -14,3 +14,15 @@ support_email              = "digitalt-dodsbo-prod-aaaapupmzvtgp4ppcx7ggiaou4@di
 altinn_apps_digdir_kv_name = "digdir-prod-apps-kv"
 altinn_apps_digdir_rg_name = "altinnapps-digdir-prod-rg"
 github_action_oid          = "b59ab7b9-db38-4bd3-8614-e6d10ad70b11"
+static_whitelist = [
+    {
+      name     = "Oekern_office"
+      start_ip = "78.41.45.0"
+      end_ip   = "78.41.45.0"
+    },
+    {
+      name     = "aks"
+      start_ip = "20.251.5.58"
+      end_ip   = "20.251.5.59"
+    }
+  ]
--- infra/altinn-digdir-oed-tt02-rg/database.tf
+++ infra/altinn-digdir-oed-tt02-rg/database.tf
@@ -63,3 +63,39 @@ resource "azurerm_key_vault_secret" "user_conn_string" {
   value        = "Server=${azurerm_postgresql_flexible_server.pg.fqdn};Username=${local.app_user.name};Database=${azurerm_postgresql_flexible_server_database.oed_db.name};Port=5432;Password='${local.app_user.password}';SSLMode=Prefer"
   key_vault_id = azurerm_key_vault.kv.id
 }
+
+locals {
+  # splitter "outbound_ip_addresses" (komma-separert streng) til liste
+  authz_ips = split(",", azurerm_windows_web_app.authz.outbound_ip_addresses)  
+  feedpoller_ips    = split(",", azurerm_function_app.feedpoller.outbound_ip_addresses)
+
+  # bygg en liste av objekter med navn og IP
+  dynamic_whitelist = flatten([
+    [for ip in local.authz_ips : {
+      name     = "authz-${replace(ip, ".", "-")}"
+      start_ip = ip
+      end_ip   = ip
+    }],
+    [for ip in local.feedpoller_ips : {
+      name     = "feedpoller-${replace(ip, ".", "-")}"
+      start_ip = ip
+      end_ip   = ip
+    }],
+  ])
+
+  # kombiner statisk + dynamisk
+  all_whitelist = concat(var.static_whitelist, local.dynamic_whitelist)
+
+  # lag map for for_each
+  whitelist_map = { for rule in local.all_whitelist : rule.name => rule }
+}
+
+resource "azurerm_postgresql_flexible_server_firewall_rule" "whitelist" {
+  depends_on = [ local.whitelist_map ]
+  for_each = local.whitelist_map
+
+  name                = each.key
+  server_id = azurerm_postgresql_flexible_server.pg.id  
+  start_ip_address    = each.value.start_ip
+  end_ip_address      = each.value.end_ip
+}
