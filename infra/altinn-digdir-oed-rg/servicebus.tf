a5ecbcfa824f59c652dfe0b64ad87b2b925aa075
Kurt Monge <kurt.monge@knowit.com>
2025-05-20 11:37:56 +0200

    1193: Lagt til ip whitelisting på SB

--- infra/altinn-digdir-oed-rg/servicebus.tf
+++ infra/altinn-digdir-oed-rg/servicebus.tf
@@ -9,6 +9,17 @@ resource "azurerm_servicebus_namespace" "dd_sb_ns" {
   }
 }
 
+resource "azurerm_servicebus_namespace_network_rule_set" "sb_whitelist" {
+  name                = azurerm_servicebus_namespace.dd_sb_ns.name
+  namespace_name      = azurerm_servicebus_namespace.dd_sb_ns.name
+  resource_group_name = azurerm_servicebus_namespace.dd_sb_ns.resource_group_name
+
+  default_action                = "Deny"
+  public_network_access_enabled = true
+  trusted_services_allowed      = true
+  ip_rules                      = whitelist_array
+}
+
 # Gi full topic/subscription access til authz
 resource "azurerm_role_assignment" "sb_authz_ra" {
   scope                = azurerm_servicebus_namespace.dd_sb_ns.id
