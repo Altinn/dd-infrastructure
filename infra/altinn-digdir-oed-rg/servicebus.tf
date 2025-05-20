74b0a2036e9f5b4081de3b23692ff689c721a31a
Kurt Monge <kurt.monge@knowit.com>
2025-05-20 11:45:32 +0200

    1193: Endret oppsett av whitelisting

--- infra/altinn-digdir-oed-rg/servicebus.tf
+++ infra/altinn-digdir-oed-rg/servicebus.tf
@@ -7,17 +7,12 @@ resource "azurerm_servicebus_namespace" "dd_sb_ns" {
     costcenter = "altinn3"
     solution   = "apps"
   }
-}
-
-resource "azurerm_servicebus_namespace_network_rule_set" "sb_whitelist" {
-  name                = azurerm_servicebus_namespace.dd_sb_ns.name
-  namespace_name      = azurerm_servicebus_namespace.dd_sb_ns.name
-  resource_group_name = azurerm_servicebus_namespace.dd_sb_ns.resource_group_name
-
-  default_action                = "Deny"
-  public_network_access_enabled = true
-  trusted_services_allowed      = true
-  ip_rules                      = whitelist_array
+  network_rule_set {
+    default_action                = "Deny"
+    public_network_access_enabled = true
+    trusted_services_allowed      = true
+    ip_rules                      = whitelist_array
+  }
 }
 
 # Gi full topic/subscription access til authz
