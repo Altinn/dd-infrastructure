b88947f55e4590378d81ad3973a4c0e2a176f25a
Kurt70 <kurt.monge@gmail.com>
2025-06-03 15:40:46 +0200

    1191:format

--- infra/altinn-digdir-oed-tt02-rg/web-app-admin.tf
+++ infra/altinn-digdir-oed-tt02-rg/web-app-admin.tf
@@ -2,7 +2,7 @@ data "azuread_group" "developers" {
   display_name = var.admin_app_user_groupname
 }
 
-resource "azurerm_service_plan" "admin_asp" {  
+resource "azurerm_service_plan" "admin_asp" {
   name                = "ASP altinndigdir-dd-${var.environment}-admin"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
@@ -16,7 +16,7 @@ resource "azurerm_linux_web_app" "admin_app" {
   resource_group_name = azurerm_resource_group.rg.name
   service_plan_id     = azurerm_app_service_plan.admin_asp.id
   https_only          = true
-  tags                = {
+  tags = {
 
     "costcenter" = "altinn3"
     "solution"   = "apps"
@@ -25,7 +25,7 @@ resource "azurerm_linux_web_app" "admin_app" {
     application_stack {
       dotnet_version = "9.0"
     }
-    always_on        = false
+    always_on = false
   }
 
   identity {
@@ -33,14 +33,14 @@ resource "azurerm_linux_web_app" "admin_app" {
   }
 
   auth_settings_v2 {
-    auth_enabled            = true
-    require_authentication  = true
-    default_provider        = "azureactivedirectory"
+    auth_enabled           = true
+    require_authentication = true
+    default_provider       = "azureactivedirectory"
 
     active_directory_v2 {
-      client_id           = data.azurerm_client_config.current.client_id
+      client_id            = data.azurerm_client_config.current.client_id
       tenant_auth_endpoint = "https://login.microsoftonline.com/${var.tenant_id}/v2.0/"
-      allowed_groups = [data.azuread_group.developers.display_name]
+      allowed_groups       = [data.azuread_group.developers.display_name]
     }
 
     login {
@@ -50,7 +50,7 @@ resource "azurerm_linux_web_app" "admin_app" {
 }
 
 resource "azurerm_key_vault_access_policy" "dd_admin_read_secrets" {
-  depends_on = [ azurerm_linux_web_app.admin_app ]
+  depends_on   = [azurerm_linux_web_app.admin_app]
   key_vault_id = azurerm_key_vault.kv.id
   tenant_id    = azurerm_linux_web_app.admin_app.identity[0].tenant_id
   object_id    = azurerm_linux_web_app.admin_app.identity[0].principal_id
