6825d2bc95f317a9813fffa4e464dae5381b928f
Kurt70 <kurt.monge@gmail.com>
2025-06-03 16:31:37 +0200

    1191: format

--- infra/altinn-digdir-oed-tt02-rg/web-app-admin.tf
+++ infra/altinn-digdir-oed-tt02-rg/web-app-admin.tf
@@ -7,7 +7,7 @@ resource "azurerm_service_plan" "admin_asp" {
 }
 
 resource "azurerm_linux_web_app" "admin_app" {
-  depends_on = [ azurerm_service_plan.admin_asp ]
+  depends_on          = [azurerm_service_plan.admin_asp]
   name                = "dd-${var.environment}-admin-app"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
