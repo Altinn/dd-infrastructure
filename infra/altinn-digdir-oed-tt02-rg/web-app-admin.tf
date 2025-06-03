fd10118a1be8c25f33e89ebf092c30f4bd4bad40
Kurt70 <kurt.monge@gmail.com>
2025-06-03 15:39:50 +0200

    1191: Småendringer

--- infra/altinn-digdir-oed-tt02-rg/web-app-admin.tf
+++ infra/altinn-digdir-oed-tt02-rg/web-app-admin.tf
@@ -2,17 +2,12 @@ data "azuread_group" "developers" {
   display_name = var.admin_app_user_groupname
 }
 
-resource "azurerm_app_service_plan" "admin_asp" {
+resource "azurerm_service_plan" "admin_asp" {  
   name                = "ASP altinndigdir-dd-${var.environment}-admin"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  kind                = "Linux"
-  reserved            = true
-
-  sku {
-    tier = "Basic"
-    size = "B1"
-  }
+  os_type             = "Linux"
+  sku_name            = "B1"
 }
 
 resource "azurerm_linux_web_app" "admin_app" {
@@ -20,10 +15,17 @@ resource "azurerm_linux_web_app" "admin_app" {
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   service_plan_id     = azurerm_app_service_plan.admin_asp.id
+  https_only          = true
+  tags                = {
 
+    "costcenter" = "altinn3"
+    "solution"   = "apps"
+  }
   site_config {
+    application_stack {
+      dotnet_version = "9.0"
+    }
     always_on        = false
-    linux_fx_version = "DOTNETCORE|9.0" # Juster til korrekt runtime
   }
 
   identity {
@@ -45,8 +47,6 @@ resource "azurerm_linux_web_app" "admin_app" {
       token_store_enabled = true
     }
   }
-
-  https_only = true
 }
 
 resource "azurerm_key_vault_access_policy" "dd_admin_read_secrets" {
