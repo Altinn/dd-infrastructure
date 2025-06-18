2c860e1d96f24edac3a9bdc7342b28dbea717e5d
Kurt Monge <kurt.monge@knowit.com>
2025-06-18 15:50:36 +0200

    1191: Lagt til start/slutt dato

--- infra/altinn-digdir-oed-tt02-rg/web-app-admin.tf
+++ infra/altinn-digdir-oed-tt02-rg/web-app-admin.tf
@@ -120,8 +120,10 @@ resource "azurerm_key_vault_access_policy" "dd_admin_read_secrets" {
 
 # Legger client secret som har utløpsdato i keyvault slik at vi kan følge opp.
 resource "azurerm_key_vault_secret" "admin_app_client_secret" {
+  depends_on      = [azuread_application_password.admin_app_secret_V2]
   name            = "dd-admin-app-client-secret"
   value           = azuread_application_password.admin_app_secret_V2.value
-  expiration_date = azuread_application_password.admin_app_secret_V2.expiration_date
+  expiration_date = azuread_application_password.admin_app_secret_V2.end_date
+  not_before_date = azuread_application_password.admin_app_secret_V2.start_date
   key_vault_id    = azurerm_key_vault.kv.id
 }
