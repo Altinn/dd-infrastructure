93e162c83c1ff0953ab9e7978acaa2d04888f7bc
Kurt Monge <kurt.monge@knowit.com>
2025-06-19 06:51:22 +0200

    1191: Lagt inn easyauth app

---
+++ infra/altinn-digdir-oed-tt02-rg/web-app-easyauth.tf
@@ -0,0 +1,23 @@
+import {
+  to = azurerm_windows_web_app.easyauth_app
+  id = "/subscriptions/7b6f8f15-3a3e-43a2-b6ac-8eb6c06ad103/resourceGroups/altinn-digdir-oed-tt02-rg/providers/Microsoft.Web/sites/dd-test-easyauth-app"
+}
+
+resource "azurerm_windows_web_app" "easyauth_app" {
+  name                = "dd-${var.environment}-easyauth-app"
+  service_plan_id     = azurerm_service_plan.authz.id
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  https_only          = true
+  site_config {
+    always_on = true
+    application_stack {
+      current_stack  = "dotnet"
+      dotnet_version = "v9.0"
+      node_version   = "~16"
+    }
+ }
+ identity {
+   type = SystemAssigned
+ }
+}
