902f7f78943420dd0e9b4398b76c86a31fea4506
Kurt Monge <kurt.monge@knowit.com>
2025-06-19 07:11:40 +0200

    1191: Div

--- infra/altinn-digdir-oed-tt02-rg/web-app-easyauth.tf
+++ infra/altinn-digdir-oed-tt02-rg/web-app-easyauth.tf
@@ -3,7 +3,7 @@ import {
   id = "/subscriptions/7b6f8f15-3a3e-43a2-b6ac-8eb6c06ad103/resourceGroups/altinn-digdir-oed-tt02-rg/providers/Microsoft.Web/sites/dd-test-easyauth-app"
 }
 
-resource "azurerm_windows_web_app" "easyauth_app" {
+resource "azurerm_linux_web_app" "easyauth_app" {
   name                = "dd-${var.environment}-easyauth-app"
   service_plan_id     = azurerm_service_plan.admin_asp.id
   resource_group_name = azurerm_resource_group.rg.name
@@ -12,9 +12,7 @@ resource "azurerm_windows_web_app" "easyauth_app" {
   site_config {
     always_on = true
     application_stack {
-      current_stack  = "dotnet"
-      dotnet_version = "v9.0"
-      node_version   = "~16"
+      dotnet_version = "8.0"
     }
   }
   identity {
