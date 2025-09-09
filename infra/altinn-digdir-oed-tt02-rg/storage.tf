f479b1765ba2c7702289772593ab269d5f9fec8a
Kurt70 <kurt.monge@gmail.com>
2025-09-09 17:28:20 +0200

    1342: flyttet authz db

--- infra/altinn-digdir-oed-tt02-rg/storage.tf
+++ infra/altinn-digdir-oed-tt02-rg/storage.tf
@@ -38,54 +38,6 @@ resource "azurerm_storage_management_policy" "sa_version_cleanup_smp" {
   }
 }
 
-
-# resource "azurerm_postgresql_flexible_server" "psql" {
-#   lifecycle {
-#     ignore_changes  = all
-#     prevent_destroy = true
-#   }
-#   administrator_login = "oed${var.environment}pgadmin"
-#   #administrator_password        = random_password.psql_oedpgadmin.result
-#   auto_grow_enabled             = false
-#   backup_retention_days         = 7
-#   location                      = var.alt_location
-#   name                          = "oed-${var.environment}-authz-pg"
-#   public_network_access_enabled = true
-#   resource_group_name           = azurerm_resource_group.rg.name
-#   sku_name                      = "B_Standard_B2s"
-#   version                       = "16"
-
-#   authentication {
-#     active_directory_auth_enabled = true
-#     password_auth_enabled         = true
-#     tenant_id                     = "cd0026d8-283b-4a55-9bfa-d0ef4a8ba21c"
-#   }
-#   tags = {
-#     "costcenter" = "altinn3"
-#     "solution"   = "apps"
-#   }
-# }
-
-# resource "azurerm_postgresql_flexible_server_database" "oedauthz" {
-#   lifecycle {
-#     ignore_changes = all
-#   }
-#   name      = "oedauthz"
-#   server_id = azurerm_postgresql_flexible_server.psql.id
-#   collation = "en_US.utf8"
-#   charset   = "utf8"
-# }
-
-# resource "azurerm_postgresql_flexible_server_firewall_rule" "authz_whitelist" {
-#   depends_on = [local.whitelist_map_pg]
-#   for_each   = local.whitelist_map_pg
-
-#   name             = each.key
-#   server_id        = azurerm_postgresql_flexible_server.psql.id
-#   start_ip_address = each.value.start_ip
-#   end_ip_address   = each.value.end_ip
-# }
-
 resource "azurerm_redis_cache" "cache" {
   name                = "oed-${var.environment}-cache"
   location            = var.alt_location
