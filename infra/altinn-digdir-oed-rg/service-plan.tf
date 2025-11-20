96018b2b7689488440be7e99f4e5b8f1481c5b15
Lars Olav Kjølstad <lars.olav.kjolstad@digdir.no>
2025-11-20 14:10:38 +0100

    Remove windows serviceplan for authz (prod)

--- infra/altinn-digdir-oed-rg/service-plan.tf
+++ infra/altinn-digdir-oed-rg/service-plan.tf
@@ -7,15 +7,6 @@ resource "azurerm_service_plan" "feedpoller" {
   worker_count        = 1
 }
 
-resource "azurerm_service_plan" "authz" {
-  name                = "ASP-altinndigdiroed-authz"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  os_type             = "Windows"
-  sku_name            = "S1"
-  worker_count        = 1
-}
-
 resource "azurerm_service_plan" "authz_linux" {
   name                = "ASP-altinndigdiroed-authz-linux"
   location            = azurerm_resource_group.rg.location
