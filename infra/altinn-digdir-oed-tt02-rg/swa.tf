edcf425f6bb04dc9d038df60f98b7f62e75e19f8
espen.lonnkvist <espen.smith.lonnkvist@digdir.no>
2026-06-03 17:07:42 +0200

    Use standard sku for swa

--- infra/altinn-digdir-oed-tt02-rg/swa.tf
+++ infra/altinn-digdir-oed-tt02-rg/swa.tf
@@ -1,5 +1,6 @@
 resource "azurerm_static_web_app" "qaswa" {
   name                 = "oed-qa-swa"
   resource_group_name  = azurerm_resource_group.rg.name
-  location              = var.alt_location
+  location             = var.alt_location
+  sku_tier             = "Standard" 
 }
