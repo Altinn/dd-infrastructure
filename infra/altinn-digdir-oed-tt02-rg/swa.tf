05cdd9750884ee96ad8200db808c48ee2b981d15
espen.lonnkvist <espen.smith.lonnkvist@digdir.no>
2026-06-04 09:36:21 +0200

    Use free tier for swa

--- infra/altinn-digdir-oed-tt02-rg/swa.tf
+++ infra/altinn-digdir-oed-tt02-rg/swa.tf
@@ -2,6 +2,6 @@ resource "azurerm_static_web_app" "qaswa" {
   name                 = "oed-qa-swa"
   resource_group_name  = azurerm_resource_group.rg.name
   location             = var.alt_location
-  sku_tier             = "Standard" 
-  sku_size             = "Standard" 
+  sku_tier             = "Free" 
+  sku_size             = "Free" 
 }
